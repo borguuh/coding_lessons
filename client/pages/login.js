1206f50057a99e528e3b4823a6032b68663f36c7
Borguuh <abdrahmanadewale@gmail.com>
2022-04-04 11:55:45 +0100

    stripe onboarding link

--- client/pages/login.js
+++ client/pages/login.js
@@ -7,7 +7,7 @@ import { Context } from "../context";
 import { useRouter } from "next/router";
 
 const Login = () => {
-  const [email, setEmail] = useState("ryan@gmail.com");
+  const [email, setEmail] = useState("Borguuh@gmail.com");
   const [password, setPassword] = useState("rrrrrr");
   const [loading, setLoading] = useState(false);
 

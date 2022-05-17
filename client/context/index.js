24434ea94b2ef422bfcf37491739ca3311af171a
Borguuh <abdrahmanadewale@gmail.com>
2022-05-17 20:12:12 +0100

    remove axios interceptor

--- client/context/index.js
+++ client/context/index.js
@@ -36,39 +36,41 @@ const Provider = ({ children }) => {
     });
   }, []);
 
-  axios.interceptors.response.use(
-    function (response) {
-      // any status code that lie within the range of 2XX cause this function
-      // to trigger
-      return response;
-    },
-    function (error) {
-      // any status codes that falls outside the range of 2xx cause this function
-      // to trigger
-      let res = error.response;
-      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
-        return new Promise((resolve, reject) => {
-          axios
-            .get(`${process.env.NEXT_PUBLIC_API}/logout`)
-            .then((data) => {
-              console.log("/401 error > logout");
-              dispatch({ type: "LOGOUT" });
-              window.localStorage.removeItem("user");
-              router.push("/login");
-            })
-            .catch((err) => {
-              console.log("AXIOS INTERCEPTORS ERR", err);
-              reject(error);
-            });
-        });
-      }
-      return Promise.reject(error);
-    }
-  );
+  // axios.interceptors.response.use(
+  //   function (response) {
+  //     // any status code that lie within the range of 2XX cause this function
+  //     // to trigger
+  //     return response;
+  //   },
+  //   function (error) {
+  //     // any status codes that falls outside the range of 2xx cause this function
+  //     // to trigger
+  //     let res = error.response;
+  //     if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+  //       return new Promise((resolve, reject) => {
+  //         axios
+  //           .get(`${process.env.NEXT_PUBLIC_API}/logout`)
+  //           .then((data) => {
+  //             console.log("/401 error > logout");
+  //             dispatch({ type: "LOGOUT" });
+  //             window.localStorage.removeItem("user");
+  //             router.push("/login");
+  //           })
+  //           .catch((err) => {
+  //             console.log("AXIOS INTERCEPTORS ERR", err);
+  //             reject(error);
+  //           });
+  //       });
+  //     }
+  //     return Promise.reject(error);
+  //   }
+  // );
 
   useEffect(() => {
     const getCsrfToken = async () => {
-      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/csrf-token`);
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API}/csrf-token`
+      );
       // console.log("CSRF", data);
       axios.defaults.headers["X-CSRF-Token"] = data.csrfToken;
     };
